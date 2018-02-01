3dbe554f07f6ea9719d2a4b74a80a1a41f495a3e
Sidharth Sharma <sidsharma27@users.noreply.github.com>
2018-02-01 11:51:01 -0800

    Update DemoServer.js

--- DemoServer.js
+++ DemoServer.js
@@ -1,5 +1,3 @@
-//var fs = require('fs');
-//var https = require('https');
 var http = require('http');
 var express = require('express');
 var AgoraSignGenerator = require('./AgoraSignGenerator');
@@ -8,10 +6,6 @@ var favicon = require('serve-favicon');
 var AgoraSignalingKeyGenerator = require('./SignalingToken');
 const md5 = require("md5");
 var PORT = 8080;
-
-//var private_key = fs.readFileSync(__dirname + '/../../cert/xxx.com.key');
-//var certificate = fs.readFileSync(__dirname + '/../../cert/xxx.com.crt');
-//var credentials = {key: private_key, cert: certificate, passphrase: "password"};
 
 var app = express();
 app.disable('x-powered-by');
@@ -30,7 +24,6 @@ var generateDynamicKey = function(req, resp) {
     var key = AgoraSignGenerator.generateDynamicKey("Agora-App-ID", "Agora-App-Certificate", channelName, ts, rnd);
 
     resp.header("Access-Control-Allow-Origin", "*")
-    //resp.header("Access-Control-Allow-Origin", "http://ip:port")
     return resp.json({ 'key': key }).send();
 };
 
@@ -50,37 +43,9 @@ var generateSignalingKey = function(req, resp) {
   return resp.json({ 'signalingToken': signalingToken }).send();
 };
 
-
-/*
-var generateSignalingKey = function(appid, appcertificate, account, validTimeInSeconds){
-    var expiredTime = parseInt(new Date().getTime() / 1000)+ validTimeInSeconds;
-    var token_items = [];
-
-    //append SDK VERSION
-    token_items.push("1");
-
-    //append appid
-    token_items.push(appid);
-
-    //expired time
-    token_items.push(expiredTime);
-
-    //md5 account + appid + appcertificate + expiredtime
-    token_items.push(md5("account" + "appid" + "appcertificate" + expiredTime));
-
-    return token_items.join(":");
-};
-*/
-
-
-
 app.get('/dynamic_key', generateDynamicKey);
 app.get('/signaling_key', generateSignalingKey);
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('AgoraSignServer starts at ' + app.get('port'));
 });
-
-//https.createServer(credentials, app).listen(app.get('port') + 1, function() {
-//    console.log('AgoraSignServer starts at ' + (app.get('port') + 1));
-//});
